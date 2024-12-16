64683e80f012b0019e1d9161e3d83bf302cdf336
jimtyhurst <jimtyhurst@gmail.com>
2024-12-16 02:04:55 +0000

    Added vscode extensions to nix configuration.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -26,6 +26,8 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "ms-python.python"
+      "ms-toolsai.jupyter"
     ];
 
     # Enable previews
